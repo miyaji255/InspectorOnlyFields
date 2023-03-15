77a7146e8efc8461bafcd73a020259d935623c3c
yu195 <miyayuu.since2018@gmail.com>
2023-03-15 17:43:48 +0900

    使えるようにした

---
+++ InspectorOnlyFields/Assets/Scripts/SampleObject.cs
@@ -0,0 +1,20 @@
+using InspectorOnlyFields;
+using UnityEngine;
+
+public class SampleObject : MonoBehaviour
+{
+    [InspectorOnly]
+    public GameObject GameObject = new GameObject();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
