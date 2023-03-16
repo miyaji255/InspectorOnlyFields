19aff583582360effa8085d6fbd011c73a1aa9b5
yu195 <miyayuu.since2018@gmail.com>
2023-03-16 09:50:34 +0900

    InspectorOnlyAttributeをSourceGeneratorを使わないように変更

--- InspectorOnlyAnalyzer/InspectorOnlyAnalyzer/InspectorOnlyAnalyzer/InspectorOnlyAttributesGenerator.cs
+++ InspectorOnlyAnalyzer/InspectorOnlyAnalyzer/InspectorOnlyAnalyzer/InspectorOnlyAttributesGenerator.cs
@@ -2,17 +2,9 @@
 
 namespace InspectorOnlyAnalyzer
 {
-    [Generator]
-    public partial class InspectorOnlyAttributesGenerator : ISourceGenerator
+    public class InspectorOnlyAttributesGenerator
     {
         public static readonly string InspectorOnlyAttributeName = "InspectorOnlyFields.InspectorOnlyAttribute";
         public static readonly string[] InspectorOnlyAttributeNameArray = InspectorOnlyAttributeName.Split('.');
-
-        public void Initialize(GeneratorInitializationContext context) { }
-
-        public void Execute(GeneratorExecutionContext context) {
-            context.AddSource("InspectorOnlyAttribute.cs", _ioaSource);
-            //context.AddSource("IgnoreInspectorOnlyAttribute.cs", _ignoreSource);
-        }
     }
 }
--- InspectorOnlyFields/Packages/InspectorOnlyFields/Analyzer/Attributes/InspectorOnlyAttribute.cs
+++ InspectorOnlyFields/Packages/InspectorOnlyFields/Analyzer/Attributes/InspectorOnlyAttribute.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Packages.InspectorOnlyFields.Analyzer.NewFolder1
+namespace InspectorOnlyFields
 {
-    internal class InspectorOnlyAttribute
-    {
-    }
+    [AttributeUsage(AttributeTargets.Field)]
+    public class InspectorOnlyAttribute : Attribute { }
 }
